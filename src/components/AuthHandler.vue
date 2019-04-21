9ccc28fb8b072f521b37c4a48e0ca3478bdc238b
Jose Xavier <jrxavier@gmail.com>
2019-04-21 20:17:50 -0300

    Callin actions

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -6,7 +6,13 @@
 
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
-  name: 'AuthHandler'
+  name: 'AuthHandler',
+  method: mapActions(['finalizeLogin']),
+  created() {
+    this.finalizeLogin(window.location.hash);
+  }
 }
 </script>
