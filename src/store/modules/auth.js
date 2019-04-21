08fdfdf51fd9b06037eaf85a31da498d32a7bc60
Jose Xavier <jrxavier@gmail.com>
2019-04-21 12:49:27 -0300

    Auth module state and getters

---
+++ src/store/modules/auth.js
@@ -0,0 +1,19 @@
+const state = {
+    token: null
+};
+
+const getters = {
+    isLoggedIn: (state) => {
+        // !! é uma forma de gerar avaliação booleana  
+        !!state.token;
+    }
+};
+
+
+const actions = {
+
+};
+
+const mutations = {
+
+};
